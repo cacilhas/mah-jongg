737ab549721480015656db1c353b25ccd0c61b31
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2024-08-07 22:54:36 -0300

    rename UI module to ui

--- ui/src/lib.rs
+++ ui/src/lib.rs
@@ -7,10 +7,10 @@ mod start_scene;
 use godot::global::randomize;
 use godot::prelude::*;
 
-struct MahJonggPlugin;
+struct MahJonggUIPlugin;
 
 #[gdextension]
-unsafe impl ExtensionLibrary for MahJonggPlugin {
+unsafe impl ExtensionLibrary for MahJonggUIPlugin {
     fn on_level_init(level: InitLevel) {
         if level == InitLevel::Core {
             randomize();
